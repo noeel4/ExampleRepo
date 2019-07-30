279ad56dffbeb8fb9b4321364227dc34593f6394
Josh <josh@Joshs-MacBook-Air.local>
2019-07-29 22:10:12 -0700

    Attributed ASCII art

--- lab9.py
+++ lab9.py
@@ -8,7 +8,28 @@
 # -------------------------------------------------
 
 import random
+######################################################################
+###~~~~~~~~~~~~~~~~~~~~~~~ASCII ART~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~###
+###                Created By Joan G. Stark                        ###
+### Website: https://www.asciiart.eu/sports-and-outdoors/billiards ###
+###             we see you, professor jordan!                      ###
 
+print("""
+        ____
+    ,dP9CGG88@b,
+  ,IP  _   Y888@@b,
+ dIi  (_)   G8888@b
+dCII  (_)   G8888@@b
+GCCIi     ,GG8888@@@
+GGCCCCCCCGGG88888@@@
+GGGGCCCGGGG88888@@@@...
+Y8GGGGGG8888888@@@@P.....
+ Y88888888888@@@@@P......
+ `Y8888888@@@@@@@P'......
+    `@@@@@@@@@P'.......
+        """"........""")
+###~~~~~~~~~~~~~~~~~~~~~~~~~END ASCII ART~~~~~~~~~~~~~~~~~~~~~~~~~~###
+######################################################################
 def eightballanswers():
     read_file=open("responses.txt","r")
 
@@ -31,7 +52,7 @@ def main():
     while next_question.upper() == "YES":
         askquestion()
         next_question=input("New question? (YES or NO):")
-        
+        print('uwu?')
         
         
     
